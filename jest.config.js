bc2a6e0974b2cb6a3adb0387b059fdddf337a59c
Nguyen Tien Son <jasonbmt06@gmail.com>
2026-05-17 13:42:41 +0700

    chore: add ESLint, Prettier, Jest configs and Flutter analysis options

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.module.ts',
+    '!**/main.ts',
+    '!**/index.ts',
+    '!**/*.dto.ts',
+    '!**/*.entity.ts',
+  ],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
